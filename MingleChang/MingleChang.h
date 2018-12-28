f658523b0556e509357717ed1f7f3dd0745d816e
Mingle <mingle890805@gmail.com>
2018-12-28 11:21:59 +0800

    Initial commit

---
+++ MingleChang/MingleChang.h
@@ -0,0 +1,19 @@
+//
+//  MingleChang.h
+//  MingleChang
+//
+//  Created by gongtao on 2018/12/28.
+//  Copyright © 2018 mingle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MingleChang.
+FOUNDATION_EXPORT double MingleChangVersionNumber;
+
+//! Project version string for MingleChang.
+FOUNDATION_EXPORT const unsigned char MingleChangVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MingleChang/PublicHeader.h>
+
+
