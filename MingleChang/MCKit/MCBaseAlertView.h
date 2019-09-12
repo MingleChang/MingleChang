b49cfb1375b6b6d6aa6a7a245e31277f97a9a9ea
MingleChang <mingle890805@gmail.com>
2019-09-12 11:38:30 +0800

    not

--- MingleChang/MCKit/MCBaseAlertView.h
+++ MingleChang/MCKit/MCBaseAlertView.h
@@ -80,7 +80,11 @@ NS_ASSUME_NONNULL_BEGIN
 @end
 
 @interface MCAlertManager : NSObject
+
 @property (nonatomic, copy)UIView *(^superViewBlock)(void);
+
++ (MCAlertManager *)manager;
+
 @end
 
 NS_ASSUME_NONNULL_END
